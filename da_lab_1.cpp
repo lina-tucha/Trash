86c897ab99964e591b192f0df1c8c6afd7e7dff2
lina-tucha <71285415+lina-tucha@users.noreply.github.com>
2020-10-03 01:44:13 +0300

    Add files via upload

---
+++ da_lab_1.cpp
@@ -0,0 +1,202 @@
+#include <iostream>
+#include <cstdint>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <chrono>
+#include <cmath>
+
+template <typename T>
+class TVector {
+public:
+    TVector() = default;
+
+    TVector(size_t newSize)
+        : data(new T[newSize]), size(newSize), capacity(newSize) {}
+
+    TVector(size_t newSize, T defaultVal)
+        : data(new T[newSize]), size(newSize), capacity(newSize) {
+        for (size_t i = 0; i < size; ++i) {
+            data[i] = defaultVal;
+        }
+    }
+
+    TVector(const TVector& other)
+        : data(new T[other.size]), size(other.size), capacity(other.size) {
+        std::copy(other.data, other.data + other.size, data);
+    }
+
+    TVector(TVector&& other)
+        : data(other.data), size(other.size), capacity(other.capacity) {
+        other.data = nullptr;
+    }
+
+    ~TVector() {
+        delete[] data;
+    }
+
+    T& operator[] (size_t index) {
+        return data[index];
+    }
+
+    const T& operator[] (size_t index) const {
+        return data[index];
+    }
+
+    void PushBack(const T& elem) {
+        if (size == capacity) {
+            size_t newCap = capacity == 0 ? 1 : capacity * 2;
+            T* temp = new T[newCap];
+            std::copy(data, data + size, temp);
+            delete[] data;
+            data = temp;
+            capacity = newCap;
+        }
+        data[size] = elem;
+        size++;
+    }
+
+    //begin и end будут с маленькой буквы, потому что иначе не будет работать range-based for
+    /*T* begin() {
+        return data;
+    }
+
+    T* end() {
+        return data + size;
+    }*/
+
+    const T* begin() const {
+        return data;
+    }
+
+    const T* end() const {
+        return data + size;
+    }
+
+    TVector& operator=(TVector& other) {
+        if (&other == this) {
+            return *this;
+        }
+        if (other.size <= capacity) {
+            std::copy(other.data, other.data + other.size, data);
+            size = other.size_;
+        }
+        else {
+            TVector<T> tmp(other);
+            std::swap(tmp.data, data);
+            std::swap(tmp.size, size);
+            std::swap(tmp.capacity, capacity);
+        }
+        return *this;
+    }
+
+    TVector& operator = (TVector&& other) {
+        if (&other == this) {
+            return *this;
+        }
+        delete[] data;
+        data = other.data;
+        other.data = nullptr;
+        size = other.size;
+        other.size = 0;
+        capacity = other.capacity;
+        other.capacity = 0;
+        return *this;
+    }
+
+    void ShrinkToFit() {
+        if (size < capacity) {
+            capacity = size;
+            T* temp = new T[size];
+            std::copy(data, data + size, temp);
+            delete[] data;
+            data = temp;
+        }
+    }
+
+    size_t Size() const {
+        return size;
+    }
+
+private:
+    T* data = nullptr;
+    size_t size = 0;
+    size_t capacity = 0;
+};
+
+struct TSortType {
+    std::uint64_t key;
+    std::string value;
+};
+
+bool operator < (TSortType lhs, TSortType rhs) {
+    return lhs.key < rhs.key;
+}
+
+std::istream& operator >> (std::istream& is, TSortType& elem) {
+    return is >> elem.key >> elem.value;
+}
+
+std::ostream& operator << (std::ostream& is, TSortType& elem) {
+    //return is << std::setw(20) << elem.key << '\t' << std::setw(64) << elem.value;
+    return is << elem.key << ' ' << elem.value;
+}
+
+TVector<TSortType> RadixSort(TVector<TSortType> v, std::uint64_t maxKey)
+{
+    std::uint64_t k = v.Size();
+    std::uint64_t h = pow(10, 19);
+    for (std::uint64_t exp = 1; maxKey / exp > 0 && exp <= h; exp *= 10) {
+
+        int i, count[10] = { 0 };
+
+        TVector<TSortType> res(k);
+
+        for (i = 0; i < k; i++) {
+            count[(v[i].key / exp) % 10]++;
+            res[i] = v[i];
+        }
+
+        for (i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        for (i = k - 1; i >= 0; i--) {
+            res[count[(v[i].key / exp) % 10] - 1] = v[i];
+            count[(v[i].key / exp) % 10]--;
+        }
+
+        for (i = 0; i < k; i++) {
+            v[i] = res[i];
+        }
+
+        if (exp == h)
+            break;
+    }
+    return v;
+}
+
+int main() {
+
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    TVector<TSortType> inputVector;
+    TSortType temp;
+    std::uint64_t maxKey = 0;
+    while (std::cin >> temp) {
+        inputVector.PushBack(temp);
+        if (temp.key > maxKey) {
+            maxKey = temp.key;
+        }
+    }
+
+    inputVector.ShrinkToFit();
+    //using std::chrono::steady_clock;
+    //steady_clock::time_point start = steady_clock::now();
+    TVector<TSortType> result = RadixSort(inputVector, maxKey);
+    for (TSortType i : result) {
+        std::cout << i << "\n";
+    }
+    //steady_clock::duration dur = steady_clock::now() - start;
+    //std::cerr << "Countig_sort_time: " << std::chrono::duration_cast<std::chrono::milliseconds>(dur).count() << "ms\n";
+    return 0;
+}
